a28cd235c2544b39d8fa9100c21c9f97fd5c07d3
artyuhovs <artyuhovs@gmail.com>
2016-01-29 12:59:46 +0300

    Создан модуль database.room

--- backend/adder/test/adder/core_test.clj
+++ backend/adder/test/adder/core_test.clj
@@ -1,3 +1,5 @@
 (ns adder.core-test
   (:require [clojure.test :refer :all]
-            [database.core :refer :all]))
+            [database.core :refer :all]
+            [database.rooms :refer :all]
+            [database.users :refer :all]))
