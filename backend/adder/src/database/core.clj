0f90c382f78278353ededcc58b7290ca0a6fb5aa
artyuhovs <artyuhovs@gmail.com>
2016-02-07 16:54:51 +0300

    Добавил database.games. Необходимо написать тесты на игры

--- backend/adder/src/database/core.clj
+++ backend/adder/src/database/core.clj
@@ -22,300 +22,160 @@
                  :make-pool?  true}))
 (kdb/defdb korma-db db)
 
-;; === MAIN_DB_FUNCTIONs ===
-(defn select-col
-  "return specific column from table"
-  [db-spec select-map]
-    (jdbc/query db-spec [(str "select " (name (:col-name select-map))
-                              " from "  (name (:table-name select-map)))]))
-
-;;korma-mod
-(defn k-select-col
-  [select-map]
-  (let [t-n (:table-name select-map)
-        c-n (:col-name   select-map)]
-  (k/select t-n
-            (k/fields c-n))))
-
-(defn select-all
-  "return all values from table "
-  [db-spec table-name]
-  (select-col db-spec (u/sel-n-upd-map (name table-name) "*")))
-
-;;korma-mod
-(defn k-select-all
-  [table-name]
-  (k/select table-name))
-
-(defn select-col-by-field
-  "return specific column from table where field-name = field-val"
-  [db-spec select-map]
-  (jdbc/query db-spec
-              [(str "select " (name (:col-name   select-map))
-                    " from "  (name (:table-name select-map))
-                    " where " (name (:field-name select-map)) " = ?")
-               (:field-val select-map)]))
-;;korma-mod
-(defn k-select-col-by-field
-  [select-map]
-  (let [t-n (:table-name select-map)
-        c-n (:col-name select-map)
-        f-n (:field-name select-map)
-        f-v (:field-val select-map)]
-    (k/select t-n
-              (k/fields c-n)
-              (k/where (= f-n f-v)))))
-;(k-select-col-by-field  (u/sel-n-upd-map :fruit :name :cost 2000))
-
-(defn select-all-by-field
-  "return all values from specified table with col-id-name and id-value"
-  [db-spec select-map]
-  (select-col-by-field db-spec (u/sel-n-upd-map (:table-name select-map)
-                                                "*"
-                                                (:field-name select-map)
-                                                (:field-val select-map))))
-;;korma-mod
-(defn k-select-all-by-field
-  [select-map]
-  (let [t-n (:table-name select-map)
-   f-n (:field-name select-map)
-   f-v (:field-val select-map)]
-  (k/select t-n
-            (k/where (= f-n f-v)))))
-;(k-select-all-by-field (u/sel-n-upd-map :users :id_user 2))
-
-;;==DEPRICATED
-(defn select-cols-multi-cond
-  "on input:
-    vector of columns to select
-    vector of maps in where: {:col-name :operation :col-val}
-  return select"
-  [db-spec table-name col-name-vec cond-map-array]
-  (let [select-col-names  (u/vec->str-with-delimiter col-name-vec ", ")
-        where-col-names   (u/vec-map->vec-by-key cond-map-array :field-name)
-        where-operation   (u/vec-map->vec-by-key cond-map-array :operation)
-        where-col-val     (u/vec-map->vec-by-key cond-map-array :field-val)
-        where-cond        (u/concat-vec->str-vec where-operation where-col-val)
-        w-cond            (u/concat-vec->str where-col-names where-cond " and ")]
-    (jdbc/query db-spec
-                [(str "select " select-col-names
-                      " from " table-name
-                      " where " w-cond)])))
-
-(defn insert-data
- "insert data (new-record-map) to the table (table-name-key)"
- [db-spec table-name new-record-map]
- (jdbc/insert! db-spec
-               (name table-name)
-               new-record-map))
-
-;;korma-mod
-(defn k-insert-data
-  [table-name new-record-map]
-  (k/sql-only
-  (k/insert table-name
-            (k/values new-record-map))))
-;(k-insert-data :fruit {:name "Забор" :appearance "Острый" :cost 100 :flag true})
-
-(defn update-data
-  "update string (update-record-map) in the table (table-name-key) where update-cond-map (:table-name :field-name field-val)"
-  [db-spec update-record-map update-cond-map]
-  (jdbc/update! db-spec
-                (name (:table-name update-cond-map))
-                update-record-map
-                [(str (name (:field-name update-cond-map)) " = ? ") (:field-val update-cond-map)]))
-
-;;korma-mod
-(defn k-update-data
-  [update-record-map update-cond-map]
-  (let [t-n (:table-name update-cond-map)
-        f-n (:field-name update-cond-map)
-        f-v (:field-val update-cond-map)]
-      (k/update t-n
-                (k/set-fields update-record-map)
-                (k/where (= f-n f-v)))))
-;(k-update-data {:cost 2000} (u/sel-n-upd-map :fruit :id_name 14))
-
-(defn update-data-multi-cond
-  "update string (update-record-map) in the table (table-name-key) where conditions are in map cond-map-array"
-  [db-spec table-name update-record-map cond-map-array]
-  (let [where-col-names   (u/vec-map->vec-by-key cond-map-array :field-name)
-        where-operation   (u/vec-map->vec-by-key cond-map-array :operation)
-        where-col-val     (u/vec-map->vec-by-key cond-map-array :field-val)
-        where-cond        (u/concat-vec->str-vec where-operation where-col-val)
-        w-cond            (u/concat-vec->str where-col-names where-cond " and ")]
-    (jdbc/update! db-spec (name table-name) update-record-map [w-cond])))
-
-(defn delete-data
-  "delete data from table with some condition"
-  [db-spec update-map]
-  (jdbc/delete! db-spec
-                (name (:table-name update-map))
-                [(str (name (:field-name update-map)) " = ? ") (:field-val update-map)]))
-
-;;korma-mod
-(defn k-delete-data
-  [update-map]
-  (let [t-n (:table-name update-map)
-        f-n (:field-name update-map)
-        f-v (:field-val update-map)]
-    (k/delete t-n
-              (k/where (= f-n f-v)))))
-;(k-delete-data (u/sel-n-upd-map :users :user_name "devAer"))
-
-(defn delete-data-multi-cond
-  "delete in the table (table-name-key) where conditions are in map cond-map-array"
-  [db-spec table-name cond-map-array]
-  (let [where-col-names   (u/vec-map->vec-by-key cond-map-array :field-name)
-        where-operation   (u/vec-map->vec-by-key cond-map-array :operation)
-        where-col-val     (u/vec-map->vec-by-key cond-map-array :field-val)
-        where-cond        (u/concat-vec->str-vec where-operation where-col-val)
-        w-cond            (u/concat-vec->str where-col-names where-cond " and ")]
-    (jdbc/delete! db-spec
-                  (name table-name)
-                  [w-cond])))
-
-
-
-;;======================== Game  Functions =====================================
-
-;;Получаем список всех игр
-(defn get-all-games
-  "Get collection of all games ever created"
-  [db-spec]
-  (select-all db-spec :game))
-
-;;Получаем список игр с параметром isDeleted = false
-(defn get-all-active-games
-  "Get collection of all games that are currently active"
-  [db-spec]
-  (select-all-by-field db-spec (u/sel-n-upd-map  :game :is_deleted false)))
-
-;;Получем список игр с параметром isPrivate = false
-(defn get-all-public-games
-  "Get collection of all non-private games"
-  [db-spec]
-  (k/select :game
-            (k/where {:is_private false
-                       :is_active true})))
-  ;(select-cols-multi-cond db-spec
-  ;                        (u/sel-n-upd-map :game)
-  ;                        ["*"]
-  ;                        [{:field-name :is_private
-  ;                          :operation "="
-  ;                          :field-val false}
-  ;                         {:field-name :is_active
-  ;                          :operation "="
-  ;                          :field-val true}]))
-
-;;Получаем список всех типов игр
-(defn get-game-types
-  "Get all available game types"
-  [db-spec]
-  (select-all db-spec :game_type))
-
-;;Получаем список всех вариантов определенного типа игры
-(defn get-game-variants
-  [db-spec id-game-type]
-  (select-all-by-field db-spec (u/sel-n-upd-map :game_variant :id_game_type id-game-type)))
-
-;;Получаем список игр определенного типа
-(defn get-games-by-variant
-  "Get collection of games by variant"
-  [db-spec id-game-variant]
-  (select-all-by-field db-spec (u/sel-n-upd-map :game :id_game_variant id-game-variant)))
-
-;TODO: пока не знаю как писать
-;;Получаем несколько новых игр
-(defn get-new-games
-  "Get collection of n newest games"
-  [db-spec number])
-
-;;Получаем список игр конкретного автора
-(defn get-games-by-author
-  "Get collection of games by author"
-  [db-spec id-author]
-  (select-all-by-field db-spec (u/sel-n-upd-map :game :id_author id-author)))
-
-;;Получаем список игр с idOriginal = null
-(defn get-all-original-games
-  "Get all games that are not forks"
-  [db-spec]
-  (select-all-by-field db-spec (u/sel-n-upd-map :game :id_original nil)))
-
-;;Получаем список форков игры
-(defn get-game-forks
-  "Get all forks of a certain game"
-  [db-spec id-game]
-      (select-all-by-field db-spec (u/sel-n-upd-map :game :id_original id-game)))
-
-;;Получаем набор данных [GameMediaType/TypeName GameMedia/filePath] по данной игре
-(defn get-game-media
-  "Get all media for a certain game"
-  [db-spec id-game]
-      (select-all-by-field db-spec (u/sel-n-upd-map :game_media :id_game id-game)))
-
-;;Получаем набор пользователей по данной игре
-(defn get-game-users
-  "Get all users for a certain game"
-  [db-spec id-game]
-  (select-all-by-field db-spec (u/sel-n-upd-map :game_users :id_game id-game)))
-
-(defn get-game-by-id
-  "Get game data by it's id"
-  [db-spec id-game]
-  (select-all-by-field db-spec (u/sel-n-upd-map :game :id_game id-game)))
-
-(defn get-random-game
-  "Return random game"
-  [db-spec]
-  (let [public-games (get-all-public-games db-spec)
-        game-rand-num (rand-int (count public-games))
-        n-games (take game-rand-num public-games)]
-    (last n-games)))
-
-;;TODO сделать функцию проверки игры на похожесть
-(defn create-game
-  "Create new record in GAME table.
-  Search for similar game first
-  Return gameID and operation-status
-  game info - map"
-  [game-info]
-  (let [result (k/insert :game
-                         (k/values game-info))]
-    ;(println result)
-    (if (nil? (:generated_key result))
-      {:error-code    (:err-code err/create-game-error)
-       :error-desc    (str (:err-desc err/create-game-error) game-info)}
-      (:generated_key result))))
-
-;;NOT IN TO-DO LIST
-(defn approve-game
-  "Approve game by game-id"
-  [db-spec game-id])
-
-(defn change-game-info-by-id
-   "Create new record in GAME table.
-   Search for similar game first
-   Return gameID and operation-status"
-  [db-spec game-id game-info]
-  (let [result (update-data db-spec  game-info (u/sel-n-upd-map :game :id_game game-id))]
-    (if (nil? (first result))
-      {:error-code (:err-code err/change-game-info-error)
-       :error-desc (str (:err-desc err/change-game-info-error) game-id " game-info " game-info)}
-      (first result))))
-
-(defn get-random-game
-  "Return random game"
-  [db-spec]
-  (let [all-games (get-all-public-games db-spec)
-        games-count (count all-games)
-        n-games (take (rand-int games-count) all-games)]
-    (last n-games)))
-
-
-
-
+;;; === MAIN_DB_FUNCTIONs ===
+;(defn select-col
+;  "return specific column from table"
+;  [db-spec select-map]
+;    (jdbc/query db-spec [(str "select " (name (:col-name select-map))
+;                              " from "  (name (:table-name select-map)))]))
+;
+;;;korma-mod
+;(defn k-select-col
+;  [select-map]
+;  (let [t-n (:table-name select-map)
+;        c-n (:col-name   select-map)]
+;  (k/select t-n
+;            (k/fields c-n))))
+;
+;(defn select-all
+;  "return all values from table "
+;  [db-spec table-name]
+;  (select-col db-spec (u/sel-n-upd-map (name table-name) "*")))
+;
+;;;korma-mod
+;(defn k-select-all
+;  [table-name]
+;  (k/select table-name))
+;
+;(defn select-col-by-field
+;  "return specific column from table where field-name = field-val"
+;  [db-spec select-map]
+;  (jdbc/query db-spec
+;              [(str "select " (name (:col-name   select-map))
+;                    " from "  (name (:table-name select-map))
+;                    " where " (name (:field-name select-map)) " = ?")
+;               (:field-val select-map)]))
+;;;korma-mod
+;(defn k-select-col-by-field
+;  [select-map]
+;  (let [t-n (:table-name select-map)
+;        c-n (:col-name select-map)
+;        f-n (:field-name select-map)
+;        f-v (:field-val select-map)]
+;    (k/select t-n
+;              (k/fields c-n)
+;              (k/where (= f-n f-v)))))
+;;(k-select-col-by-field  (u/sel-n-upd-map :fruit :name :cost 2000))
+;
+;(defn select-all-by-field
+;  "return all values from specified table with col-id-name and id-value"
+;  [db-spec select-map]
+;  (select-col-by-field db-spec (u/sel-n-upd-map (:table-name select-map)
+;                                                "*"
+;                                                (:field-name select-map)
+;                                                (:field-val select-map))))
+;;;korma-mod
+;(defn k-select-all-by-field
+;  [select-map]
+;  (let [t-n (:table-name select-map)
+;   f-n (:field-name select-map)
+;   f-v (:field-val select-map)]
+;  (k/select t-n
+;            (k/where (= f-n f-v)))))
+;;(k-select-all-by-field (u/sel-n-upd-map :users :id_user 2))
+;
+;;;==DEPRICATED
+;(defn select-cols-multi-cond
+;  "on input:
+;    vector of columns to select
+;    vector of maps in where: {:col-name :operation :col-val}
+;  return select"
+;  [db-spec table-name col-name-vec cond-map-array]
+;  (let [select-col-names  (u/vec->str-with-delimiter col-name-vec ", ")
+;        where-col-names   (u/vec-map->vec-by-key cond-map-array :field-name)
+;        where-operation   (u/vec-map->vec-by-key cond-map-array :operation)
+;        where-col-val     (u/vec-map->vec-by-key cond-map-array :field-val)
+;        where-cond        (u/concat-vec->str-vec where-operation where-col-val)
+;        w-cond            (u/concat-vec->str where-col-names where-cond " and ")]
+;    (jdbc/query db-spec
+;                [(str "select " select-col-names
+;                      " from " table-name
+;                      " where " w-cond)])))
+;
+;(defn insert-data
+; "insert data (new-record-map) to the table (table-name-key)"
+; [db-spec table-name new-record-map]
+; (jdbc/insert! db-spec
+;               (name table-name)
+;               new-record-map))
+;
+;;;korma-mod
+;(defn k-insert-data
+;  [table-name new-record-map]
+;  (k/sql-only
+;  (k/insert table-name
+;            (k/values new-record-map))))
+;;(k-insert-data :fruit {:name "Забор" :appearance "Острый" :cost 100 :flag true})
+;
+;(defn update-data
+;  "update string (update-record-map) in the table (table-name-key) where update-cond-map (:table-name :field-name field-val)"
+;  [db-spec update-record-map update-cond-map]
+;  (jdbc/update! db-spec
+;                (name (:table-name update-cond-map))
+;                update-record-map
+;                [(str (name (:field-name update-cond-map)) " = ? ") (:field-val update-cond-map)]))
+;
+;;;korma-mod
+;(defn k-update-data
+;  [update-record-map update-cond-map]
+;  (let [t-n (:table-name update-cond-map)
+;        f-n (:field-name update-cond-map)
+;        f-v (:field-val update-cond-map)]
+;      (k/update t-n
+;                (k/set-fields update-record-map)
+;                (k/where (= f-n f-v)))))
+;;(k-update-data {:cost 2000} (u/sel-n-upd-map :fruit :id_name 14))
+;
+;(defn update-data-multi-cond
+;  "update string (update-record-map) in the table (table-name-key) where conditions are in map cond-map-array"
+;  [db-spec table-name update-record-map cond-map-array]
+;  (let [where-col-names   (u/vec-map->vec-by-key cond-map-array :field-name)
+;        where-operation   (u/vec-map->vec-by-key cond-map-array :operation)
+;        where-col-val     (u/vec-map->vec-by-key cond-map-array :field-val)
+;        where-cond        (u/concat-vec->str-vec where-operation where-col-val)
+;        w-cond            (u/concat-vec->str where-col-names where-cond " and ")]
+;    (jdbc/update! db-spec (name table-name) update-record-map [w-cond])))
+;
+;(defn delete-data
+;  "delete data from table with some condition"
+;  [db-spec update-map]
+;  (jdbc/delete! db-spec
+;                (name (:table-name update-map))
+;                [(str (name (:field-name update-map)) " = ? ") (:field-val update-map)]))
+;
+;;;korma-mod
+;(defn k-delete-data
+;  [update-map]
+;  (let [t-n (:table-name update-map)
+;        f-n (:field-name update-map)
+;        f-v (:field-val update-map)]
+;    (k/delete t-n
+;              (k/where (= f-n f-v)))))
+;;(k-delete-data (u/sel-n-upd-map :users :user_name "devAer"))
+;
+;(defn delete-data-multi-cond
+;  "delete in the table (table-name-key) where conditions are in map cond-map-array"
+;  [db-spec table-name cond-map-array]
+;  (let [where-col-names   (u/vec-map->vec-by-key cond-map-array :field-name)
+;        where-operation   (u/vec-map->vec-by-key cond-map-array :operation)
+;        where-col-val     (u/vec-map->vec-by-key cond-map-array :field-val)
+;        where-cond        (u/concat-vec->str-vec where-operation where-col-val)
+;        w-cond            (u/concat-vec->str where-col-names where-cond " and ")]
+;    (jdbc/delete! db-spec
+;                  (name table-name)
+;                  [w-cond])))
+;
+;
 ;(defn selc
 ;  [table-name col-name text]
 ;  (k/select table-name
---
+++ backend/adder/src/database/games.clj
@@ -0,0 +1,189 @@
+;;
+;;======================== Game  Functions =====================================
+;;
+(ns database.games
+  (:gen-class)
+  (:require [database.core :as core]
+            [database.users :as user]
+            [utilities.core :as u]
+            [database.errors :as err]
+            [korma.core :as k]))
+
+;;Получаем список всех игр
+(defn get-all-games
+  "Get collection of all games ever created"
+  [db-spec]
+  (k/select :game))
+
+;;Получаем список игр с параметром isDeleted = false
+(defn get-all-active-games
+  "Get collection of all games that are currently active"
+  []
+  (k/select :game
+            (k/where (= :is_deleted false))))
+
+;;Получем список игр с параметром isPrivate = false
+(defn get-all-public-games
+  "Get collection of all non-private games"
+  []
+  (k/select :game
+            (k/where {:is_private false
+                      :is_active true})))
+;;Получаем список всех типов игр
+(defn get-game-types
+  "Get all available game types"
+  []
+  (k/select :game_type))
+
+;;Получаем список всех вариантов определенного типа игры
+(defn get-game-variants
+  [game-type-id]
+  (k/select :game_variant
+            (k/where (= :id_game_type game-type-id))))
+
+;;Получаем список игр определенного типа
+(defn get-games-by-variant
+  "Get collection of games by variant"
+  [db-spec game-variant-id]
+  (k/select :game
+            (k/where (= :id_game_variant game-variant-id))))
+
+;;Получаем несколько новых игр
+(defn get-n-new-public-games
+  "Get collection of n newest games"
+  [n]
+  (k/select :game
+            (k/where
+              (and
+                (= :is_deleted false)
+                (= :is_private false)))
+            (k/order :dt_created :desc)
+            (k/limit n)))
+
+(defn get-n-new-private-games
+  "Get collection of n newest games"
+  [n]
+  (k/select :game
+            (k/where
+              (and
+                (= :is_deleted false)
+                (= :is_private true)))
+            (k/order :dt_created :desc)
+            (k/limit n)))
+
+(defn get-n-new-original-games
+  "Get collection of n newest games"
+  [n]
+  (k/select :game
+            (k/where
+              (and
+                (= :is_deleted false)
+                (= :is_fork    false)))
+            (k/order :dt_created :desc)
+            (k/limit n)))
+
+;;Получаем список игр конкретного автора
+(defn get-games-by-author
+  "Get collection of games by author"
+  [author-id]
+  (k/select :game
+            (k/where
+              (and
+                (= :id_author author-id)
+                (= :is_deleted false)))))
+
+(defn get-all-games-by-author
+  "Get collection of games by author"
+  [author-id]
+  (k/select :game
+            (k/where
+              (= :id_author author-id))))
+
+;;Получаем список игр с idOriginal = null
+(defn get-original-games
+  "Get all games that are not forks"
+  []
+  (k/select :game
+            (k/where
+              (and
+                (= :is_deleted false)
+                (= :is_fork    false)))))
+
+(defn get-all-original-games
+  "Get all games that are not forks"
+  []
+  (k/select :game
+            (k/where
+              (= :is_deleted false))))
+
+;;Получаем список форков игры
+(defn get-game-forks
+  "Get all forks of a certain game"
+  [game-id]
+  (k/select :game
+            (k/where
+              (= :is_original game-id))))
+
+;;Получаем набор данных [GameMediaType/TypeName GameMedia/filePath] по данной игре
+(defn get-game-media
+  "Get all media for a certain game"
+  [db-spec id-game]
+  (k/select :game_media
+            (k/where
+              (= :is_game game-id))))
+
+;;Получаем набор пользователей по данной игре
+(defn get-game-users
+  "Get all users for a certain game"
+  [db-spec id-game]
+  (k/select :game_users
+            (k/where
+              (= :is_game game-id))))
+
+(defn get-game-by-id
+  "Get game data by it's id"
+  [db-spec id-game]
+  (k/select :game
+            (k/where
+              (= :is_game game-id))))
+
+(defn get-random-game
+  "Return random game"
+  [db-spec]
+  (let [public-games (get-all-public-games)
+        game-rand-num (rand-int (count public-games))
+        n-games (take game-rand-num public-games)]
+    (last n-games)))
+
+;;TODO сделать функцию проверки игры на похожесть
+(defn create-game
+  "Create new record in GAME table.
+  Search for similar game first
+  Return gameID and operation-status
+  game info - map"
+  [game-info]
+  (let [result (k/insert :game
+                         (k/values game-info))]
+    ;(println result)
+    (if (nil? (:generated_key result))
+      {:error-code    (:err-code err/create-game-error)
+       :error-desc    (str (:err-desc err/create-game-error) game-info)}
+      (:generated_key result))))
+
+;;NOT IN TO-DO LIST
+(defn approve-game
+  "Approve game by game-id"
+  [game-id])
+
+(defn change-game-info-by-id
+  "Create new record in GAME table.
+  Search for similar game first
+  Return gameID and operation-status"
+  [game-id game-info]
+  (let [result (k/update :game
+                          (k/set-fields game-info)
+                          (k/where (= :id_game game-id)))]
+    (if (nil? (first result))
+      {:error-code (:err-code err/change-game-info-error)
+       :error-desc (str (:err-desc err/change-game-info-error) game-id " game-info " game-info)}
+      (first result))))
